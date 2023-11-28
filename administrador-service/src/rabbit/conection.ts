a6131b64b2df75e1f580644bb873d8251fc9f89a
Arisel-Fdez <211119@ids.upchiapas.edu.mx>
2023-11-28 10:09:20 -0600

    final

--- administrador-service/src/rabbit/conection.ts
+++ administrador-service/src/rabbit/conection.ts
@@ -1,7 +1,7 @@
 const amqp = require('amqplib');
 
 export async function createConnection() {
-  const connection = await amqp.connect('amqp://production-back.up.railway.app');
+  const connection = await amqp.connect('amqp://administrador-service.up.railway.app');
   const channel = await connection.createChannel();
   return { connection, channel };
 }
