aa56d29878f9eba84a97d73e9476b819209ab01f
Arisel-Fdez <211119@ids.upchiapas.edu.mx>
2023-11-28 00:41:33 -0600

    complement-service

--- administrador-service/src/rabbit/conection.ts
+++
@@ -1,9 +0,0 @@
-const amqp = require('amqplib');
-
-export async function createConnection() {
-  const connection = await amqp.connect('amqp://localhost');
-  const channel = await connection.createChannel();
-  return { connection, channel };
-}
-
-module.exports = { createConnection };
