bffeab090281125f7e46dc584c99587876cf089b
Arisel Fernandez Cañaveral <84791183+Arisel-Fdez@users.noreply.github.com>
2023-11-29 22:36:34 -0600

    Add files via upload
    prueba4

---
+++ src/user/infraestructure/services/rabbit.ts
@@ -0,0 +1,22 @@
+import amqp from 'amqplib';
+import { RabbitMQService } from '../../application/services/rabbit';
+
+export class RabbitMQ implements RabbitMQService {
+    // Servicio y conexciodn de rabbit
+    private connection: amqp.Connection | null = null;
+
+    async connect(): Promise<void> {
+        this.connection = await amqp.connect('amqp://open-bait-production.up.railway.app');
+    }
+
+    async publishMessage(exchange: string, routingKey: string, message: any): Promise<void> {
+        if (!this.connection) {
+            throw new Error('RabbitMQ connection not established');
+        }
+        
+        const channel = await this.connection.createChannel();
+        channel.assertExchange(exchange, 'direct', { durable: false });
+        channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(message)));
+        console.log('Mensaje enviado [Y]', message)
+    }
+}
