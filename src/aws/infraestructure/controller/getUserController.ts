2fce2fcf75895704cfcec6868e9bdf2716a0ad4f
Arisel-Fdez <211119@ids.upchiapas.edu.mx>
2023-12-04 14:39:57 -0600

    Listo

--- src/aws/infraestructure/controller/getUserController.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-
-export const getUserController = (req: Request, res: Response) => {
-  // Aquí puedes añadir lógica adicional si necesitas procesar algo antes de responder
-  res.status(200).send('OK');
-};
